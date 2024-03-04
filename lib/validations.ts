6c227d69c7b4be99e5694489c3ddec7841b039a5
WinnardArthur <winnardarthur7@gmail.com>
2024-03-04 11:28:45 +0000

    create login validation

---
+++ lib/validations.ts
@@ -0,0 +1,6 @@
+import * as z from "zod";
+
+export const LoginSchema = z.object({
+  email: z.string().email({ message: "email is required" }),
+  password: z.string().min(1, { message: "password is required" }),
+});
