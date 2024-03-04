c749d408b22359e61e3700fb8fb453d3b402bece
WinnardArthur <winnardarthur7@gmail.com>
2024-03-04 10:14:57 +0000

    create header for card wrapper

---
+++ components/header.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils";
+import { Poppins } from "next/font/google";
+
+const poppinsFont = Poppins({ subsets: ["latin"], weight: ["600"] });
+
+import React from "react";
+
+interface HeaderProps {
+  label: string;
+}
+
+export const Header = ({ label }: HeaderProps) => {
+  return (
+    <div className="w-full flex flex-col gap-y-4 items-center justify-center">
+      <h1 className={cn("text-3xl font-semibold", poppinsFont.className)}>
+        VeriAuth
+      </h1>
+      <p className="text-muted-foreground text-sm">{label}</p>
+    </div>
+  );
+};
