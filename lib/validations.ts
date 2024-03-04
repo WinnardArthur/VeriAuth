4d182212935e0f908a9ae9e9c9eec5474c26c315
WinnardArthur <winnardarthur7@gmail.com>
2024-03-04 12:24:17 +0000

    create register validation

--- lib/validations.ts
+++ lib/validations.ts
@@ -1,5 +1,13 @@
 import * as z from "zod";
 
+export const RegisterSchema = z.object({
+  name: z.string().min(1, { message: "name is required" }),
+  email: z.string().email({ message: "email is required" }),
+  password: z
+    .string()
+    .min(6, { message: "password should be more than 6 characters" }),
+});
+
 export const LoginSchema = z.object({
   email: z.string().email({ message: "email is required" }),
   password: z.string().min(1, { message: "password is required" }),
