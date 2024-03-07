b612fc33aa70cf6800810f9941fdf67f96aa6adf
WinnardArthur <winnardarthur7@gmail.com>
2024-03-07 13:07:27 +0000

    implement social logins

--- auth.config.ts
+++ auth.config.ts
@@ -1,12 +1,22 @@
 import bcryptjs from "bcryptjs";
 import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 import { LoginSchema } from "./lib/validations";
 import { getUserByEmail } from "./data/user";
 
 export default {
   providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    Github({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
     Credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
@@ -21,7 +31,10 @@ export default {
           // Later wants to use credentials login
           if (!user || !user.password) return null;
 
-          const passwordsMatch = await bcryptjs.compare(password, user.password);
+          const passwordsMatch = await bcryptjs.compare(
+            password,
+            user.password
+          );
 
           if (passwordsMatch) return user;
         }
