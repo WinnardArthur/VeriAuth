1c1230e159e695ab72b0f4740f21792f8aa5d3da
WinnardArthur <winnardarthur7@gmail.com>
2024-03-07 09:11:32 +0000

    complete login and register and middleware

---
+++ middleware.ts
@@ -0,0 +1,49 @@
+import NextAuth from "next-auth";
+
+import authConfig from "@/auth.config";
+
+const { auth } = NextAuth(authConfig);
+
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from "@/routes";
+
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return;
+  }
+
+  if (isAuthRoute) {
+    // If logged in user tries to access sign in/login in,
+    // Redirect to any protected page in this case /settings
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return;
+  }
+
+  // If user is not logged in and route is a private route
+  // Redirect user to login/signup
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/auth/login", nextUrl));
+  }
+
+  return;
+});
+
+// Optionally, don't invoke Middleware on some paths
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
---
+++ routes.ts
@@ -0,0 +1,27 @@
+/**
+ * An array of public routes
+ * These routes don't require authentication
+ * @type {string[]}
+ */
+
+export const publicRoutes = ["/"];
+
+/**
+ * An array of authenticated/protected routes
+ * These routes will redirect logged in users to /settings
+ * @type {string[]}
+ */
+export const authRoutes = ["/auth/login", "/auth/register"];
+
+/**
+ * The prefix for API authentication routes.
+ * Routes that start with this prefix are used for API authentication purposes
+ * @type {string}
+ */
+export const apiAuthPrefix = "/api/auth";
+
+/**
+ * The default redirect path after loggin in
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = "/settings";
