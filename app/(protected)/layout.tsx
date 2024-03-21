a9ad04c63fa66e45c430a0739bd90a7c60c0b58f
WinnardArthur <winnardarthur7@gmail.com>
2024-03-21 13:19:41 +0000

    test for api routes

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { Toaster } from '@/components/ui/sonner';
 
 import Navbar from "./_components/navbar";
 
 const ProtectedLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full w-full flex flex-col gap-y-10 items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+      <Toaster />
       <Navbar />
       {children}
+
     </div>
   );
 };
